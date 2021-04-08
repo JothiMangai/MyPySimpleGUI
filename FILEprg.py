366d9ac427457459643e1669c540edf789f6068c
JothiMangai <73639876+JothiMangai@users.noreply.github.com>
2021-04-08 12:05:24 +0530

    Update FILEprg.py
    basic file moudule

--- FILEprg.py
+++ FILEprg.py
@@ -1,19 +1,11 @@
 def fw(fn,tasks):
     with open (fn,"w")as fp:
         for x in tasks:
-<<<<<<< HEAD
             fp.write("%s \n"% x)
-=======
-            fp.write("%s \n "% x)
->>>>>>> 599b6fb00a94da06ceb0a9cac76a8dacd6685c15
 
 def fr(fn):
     tasks=[]
     with open(fn,"r")as fp:
         lines=fp.readlines()
         for line in lines:
-<<<<<<< HEAD
-            tasks.append(line.strip("\n"))
-=======
-            tasks.append(line.strip("\n"))
->>>>>>> 599b6fb00a94da06ceb0a9cac76a8dacd6685c15
+            tasks.append(line.strip("\n")
