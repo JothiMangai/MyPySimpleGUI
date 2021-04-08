43f829aafdee12b72fff96d6ac8f287fe36caa84
JothiMangai <73639876+JothiMangai@users.noreply.github.com>
2021-04-08 11:48:25 +0530

    code for a wordbot is added

--- FILEprg.py
+++ FILEprg.py
@@ -1,11 +1,11 @@
 def fw(fn,tasks):
     with open (fn,"w")as fp:
         for x in tasks:
-            fp.write(x)
+            fp.write("%s \n"% x)
 
 def fr(fn):
     tasks=[]
     with open(fn,"r")as fp:
         lines=fp.readlines()
         for line in lines:
-            tasks.append(line)
+            tasks.append(line.strip("\n"))
